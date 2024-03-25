040eed0d58dd340f794c0b9ede38342062a65dec
paulusminus <info@paulmin.nl>
2024-03-25 22:06:29 +0100

    use tracing instead of println or eprintln

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use std::error::Error;
 use tokio::{net::TcpListener, task::JoinSet};
 use tower::ServiceBuilder;
 use tower_http::services::ServeDir;
+use tracing::{error, info};
 
 mod configuration;
-// mod shutdown;
 
 const USE_IPV6: bool = true;
 
@@ -30,31 +30,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let listen_addr = listen_address(USE_IPV6, port);
     let tcp_listener = TcpListener::bind(&listen_addr).await?;
-    println!("listening on http://{}", &listen_addr);
+    info!("listening on http://{}", &listen_addr);
 
     let mut join_set = JoinSet::new();
     loop {
         let (stream, addr) = match tcp_listener.accept().await {
             Ok(x) => x,
             Err(e) => {
-                eprintln!("failed to accept connection: {e}");
+                error!("failed to accept connection: {e}");
                 continue;
             }
         };
 
         let service = hyper_service.clone();
         let serve_connection = async move {
-            println!("handling a request from {addr}");
+            info!("handling a request from {addr}");
 
             let result = Builder::new(TokioExecutor::new())
                 .serve_connection(TokioIo::new(stream), service)
                 .await;
 
             if let Err(e) = result {
-                eprintln!("error serving {addr}: {e}");
+                error!("error serving {addr}: {e}");
             }
 
-            println!("handled a request from {addr}");
+            info!("handled a request from {addr}");
         };
 
         join_set.spawn(serve_connection);
